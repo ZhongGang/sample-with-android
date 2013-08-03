091d62e0444f70e578c132408716a24197e8d340
ZhongGang <120859274@qq.com>
2013-08-03 18:02:18 +0800

    a simple android application created by IntelliJ
    forward between activities
    Activity is a page to display content

---
+++ src/com/example/sample_with_android/LoginForm.java
@@ -0,0 +1,38 @@
+package com.example.sample_with_android;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginForm extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Button loginBtn = (Button) findViewById(R.id.login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(LoginForm.this).setTitle(R.string.tip).setMessage(R.string.login).show();
+            }
+        });
+
+        Button redirectBtn = (Button) findViewById(R.id.redirect);
+        redirectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(LoginForm.this, ShopForm.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ src/com/example/sample_with_android/ShopForm.java
@@ -0,0 +1,43 @@
+package com.example.sample_with_android;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ZhongGang
+ * Date: 13-8-3
+ * Time: 下午4:36
+ */
+public class ShopForm extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shop);
+
+        Button saveBtn = (Button) findViewById(R.id.save);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(ShopForm.this).setTitle(R.string.tip).setMessage(R.string.save).show();
+            }
+        });
+
+        Button backBtn = (Button) findViewById(R.id.back);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(ShopForm.this, LoginForm.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
